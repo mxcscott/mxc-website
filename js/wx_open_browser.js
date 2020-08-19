7b82ed578878cd087607b277f07a7d8aa3f761d4
Sheen <hx.sheen@gmail.com>
2020-08-19 11:57:01 +0200

    add wechat mask

---
+++ js/wx_open_browser.js
@@ -0,0 +1,25 @@
+(function(){
+    function init() {
+        var ua = navigator.userAgent,
+        isWeixin = !!/MicroMessenger/i.test(ua);
+    
+        var downloadApkDom = document.getElementById('downloadApk'),
+            maskTipDom = document.getElementById('maskTip');
+
+
+        if(isWeixin){
+            downloadApkDom.onclick = function(e){
+                window.event ? window.event.returnValue = false : e.preventDefault();
+                maskTipDom.style.display = 'block';
+            }
+    
+            maskTipDom.onclick = function(e){
+                this.style.display = 'none';
+            }
+        }
+    }
+
+    window.onload = function(){
+        init();
+    }
+})();
