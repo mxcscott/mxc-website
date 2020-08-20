121fce02ddfd9d362b4fafa98e46f3c20000e11a
jeffstahlnecker <45363541+jeffstahlnecker@users.noreply.github.com>
2020-08-20 16:44:00 +0200

    New translations wx_open_browser.js (Chinese Traditional)

---
+++ zh-Hant/wx_open_browser.js
@@ -0,0 +1,18 @@
+(function () {
+    function init() {
+        var ua = navigator.userAgent.toLowerCase();
+        var isWeixin = ua.indexOf('micromessenger') != -1;
+        var downloadApkDom = document.getElementById('downloadApk'), maskTipDom = document.getElementById('maskTip');
+        downloadApkDom.onclick = function (e) {
+            if (isWeixin) {
+                maskTipDom.style.display = 'block';
+            } else {
+                window.location.href = 'https://datadash.oss-accelerate.aliyuncs.com/app-prod-release.apk';
+            }
+        };
+        maskTipDom.onclick = function (e) {
+            this.style.display = 'none';
+        };
+    }
+    window.onload = init;
+}());
